466acd5b717bde5d30c40d965c5b2c31cc5d83a4
Yuri Resende <yresendeo@gmail.com>
2023-04-07 12:12:02 -0300

    Parte 2 - BD/Models e área administrativa

--- tutorialDjango/polls/models.py
+++ tutorialDjango/polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+class Question(models.Model):
+    text = models.CharField("Texto", max_length=200)
+    date = models.DateTimeField("data de publicação")
+
+    def __str__(self):
+        return self.text
+    
+    def was_published_recently(self):
+        return self.date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField("Texto", max_length=200)
+    votes = models.IntegerField("número de votos", default=0)
+
+    def __str__(self):
+        return self.choice_text
