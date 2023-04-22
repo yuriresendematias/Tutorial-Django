a8d7236c850a4a927c0fe36a6f8f549d4639381f
Yuri Resende <yresendeo@gmail.com>
2023-04-21 22:30:27 -0300

    Criando o primeiro app django e configurações iniciais de templates

--- Dashboard/Dashboard/settings.py
+++ Dashboard/Dashboard/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -116,9 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL      = 'static/'
-STATICFILES_DIR = (os.path.join(BASE_DIR, 'templates/static'),)
-STATIC_ROOT     = os.path.join('static')
+STATIC_URL       = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT      = os.path.join('static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
